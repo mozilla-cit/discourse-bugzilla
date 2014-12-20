8d34b05989e42c5fd3ccaf0654b46cfdddccd282
Leo McArdle <leomcardle@gmail.com>
2014-12-20 16:51:13 +0000

    non-working attempt at adding bug status to <a> title [issue #4]

--- assets/js/bugzilla-linker.js
+++ assets/js/bugzilla-linker.js
@@ -9,3 +9,25 @@ Discourse.Dialect.inlineRegexp({
     return ['a', {href: Discourse.SiteSettings.bugzilla_url + '/show_bug.cgi?id=' + number}, bug];
   }
 });
+
+var regex = /^bug (\d+)$/;
+
+Discourse.Dialect.on("parseNode", function(event) {
+  var node = event.node;
+  var path = event.path;
+  
+  // We only care about links
+  if (node[0] !== 'a') return;
+  
+  match = regex.exec(node[2]);
+  
+  if (match == null) return;
+  
+  node[1].title = "not what I want"
+  
+  Discourse.ajax({
+    url: Discourse.SiteSettings.bugzilla_url + '/rest/bug/' + match[1] + '?include_fields=assigned_to,status,summary'
+  }).then(function(data) {
+    node[1].title = data.bugs[0].status + ', ' + data.bugs[0].assigned_to + ' - ' + data.bugs[0].summary;
+  });
+});
