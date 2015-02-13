173d979f52d2fcbe1690176bb40d431f5d0fede8
Leo McArdle <leomcardle@gmail.com>
2015-02-13 19:59:16 +0000

    bump version number to 1.0

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-bugzilla
 # about: Discourse plugin to make it play nice with Bugzilla
-# version: 0.1
+# version: 1.0
 # authors: Leo McArdle
 
 register_asset "js/bugzilla-linker.js", :server_side
