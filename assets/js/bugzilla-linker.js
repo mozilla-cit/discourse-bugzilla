5a5bd35c6b5aab8007aefd76e976e497461d4a9c
Leo McArdle <leomcardle@gmail.com>
2015-02-13 19:48:01 +0000

    added case insensitivity [fixes #3]

--- assets/js/bugzilla-linker.js
+++ assets/js/bugzilla-linker.js
@@ -1,11 +1,30 @@
+var matcher = /^(bug (\d+))(?!\w)/gmi;
+var emitter = function(matches) {
+  var bug = matches[1]; // e.g. bug 12345
+  var number = matches[2] // e.g. 12345
+  return ['a', {href: Discourse.SiteSettings.bugzilla_url + '/show_bug.cgi?id=' + number}, bug];
+}
+
 Discourse.Dialect.inlineRegexp({
   start: 'bug',
-  matcher: /^(bug (\d+))(?!\w)/gm,
+  matcher: matcher,
   wordBoundary: true,
   
-  emitter: function(matches) {
-    var bug = matches[1]; // e.g. bug 12345
-    var number = matches[2] // e.g. 12345
-    return ['a', {href: Discourse.SiteSettings.bugzilla_url + '/show_bug.cgi?id=' + number}, bug];
-  }
+  emitter: emitter
+});
+
+Discourse.Dialect.inlineRegexp({
+  start: 'Bug',
+  matcher: matcher,
+  wordBoundary: true,
+  
+  emitter: emitter
+});
+
+Discourse.Dialect.inlineRegexp({
+  start: 'BUG',
+  matcher: matcher,
+  wordBoundary: true,
+  
+  emitter: emitter
 });
